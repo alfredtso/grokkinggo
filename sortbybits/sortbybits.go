bbef910e3b809dfb21825dea3f35fddaff749598
Alfred Tso tp <alfredtso@gmail.com>
2023-09-17 11:52:22 +0800

    LC1356

--- sortbybits/sortbybits.go
+++ sortbybits/sortbybits.go
@@ -1,8 +1,45 @@
 package sortbybits
 
-import "fmt"
+import (
+	"fmt"
+	"math/bits"
+	"slices"
+	"sort"
+)
 
 func Hello(name string) string {
 	message := fmt.Sprintf("Hello %s", name)
 	return message
+}
+
+func sortByBits(arr []int) []int {
+	// make a map having key as number of 1s in binary representation of number
+	m := make(map[int][]int)
+
+	for i := 0; i < len(arr); i++ {
+		count := bits.OnesCount(uint(arr[i]))
+		// Common idiom to check if key exists in map
+		if _, ok := m[count]; ok {
+			m[count] = append(m[count], arr[i])
+			slices.Sort(m[count])
+		} else {
+			m[count] = []int{arr[i]}
+		}
+	}
+
+	var keys []int
+
+	for k, _ := range m {
+		keys = append(keys, k)
+		sort.Ints(m[k])
+	}
+
+	var res []int
+
+	for i := 0; i < len(keys); i++ {
+		res = append(res, m[keys[i]]...)
+	}
+
+	return res
+
 }
--- sortbybits/sortbybits_test.go
+++ sortbybits/sortbybits_test.go
@@ -1,6 +1,9 @@
 package sortbybits
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSortByBits(t *testing.T) {
 	res := Hello("alfred")
@@ -8,4 +11,21 @@ func TestSortByBits(t *testing.T) {
 		t.Errorf("Hello() = %s; want Hello alfred", res)
 	}
 
+}
+
+func TestSortBits1(t *testing.T) {
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	res := sortByBits(arr)
+	expected := []int{0, 1, 2, 4, 8, 3, 5, 6, 7}
+	if reflect.DeepEqual(res, expected) != true {
+		t.Errorf("sortByBits() = %d; want [0, 1, 2, 4, 8, 3, 5, 6, 7]", res)
+	}
+}
+func TestSortBits2(t *testing.T) {
+	arr := []int{10, 100, 1000, 10000}
+	res := sortByBits(arr)
+	expected := []int{10, 100, 1000, 10000}
+	if reflect.DeepEqual(arr, expected) != true {
+		t.Errorf("sortByBits() = %d; want [10 100 10000 1000]", res)
+	}
 }
